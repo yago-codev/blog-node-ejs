a5aa4bcfd0c76cd570b76e907e597360be9c6e3f
yago-codev <yagomilano92@gmail.com>
2020-05-16 16:18:58 -0300

    Criando a rota de captura e persistencia dos dados do formulário de cadastro de novas categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const slugify = require("slugify");
+
+const Category = require("./Category");
 
 const router = express.Router();
 
 router.get("/admin/categories/new", (req, res) => {
   res.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req, res) => {
+  const title = req.body.title;
+
+  if (!title) {
+    res.redirect("admin/categories/new");
+  }
+
+  Category.create({
+    title: title,
+    slug: slugify(title),
+  }).then(() => {
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
