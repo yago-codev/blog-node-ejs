acba17d42885894cc78e59d1e188bb500a101b32
yago-codev <yagomilano92@gmail.com>
2020-05-16 05:05:28 -0300

    Criando a view de Cadastro de Categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -2,12 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/categories", (req, res) => {
-  res.send("Rota de Categorias");
-});
-
 router.get("/admin/categories/new", (req, res) => {
-  res.send("Rota para Criar Nova Categoria");
+  res.render("admin/categories/new");
 });
 
 module.exports = router;
